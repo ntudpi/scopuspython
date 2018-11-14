7ec34d3eab2dae5943c9c7f9824eaa882064478c
kevinwinatamichael <kevinwinatamichael@gmail.com>
2018-11-14 15:31:06 +0800

    change pullDOI for docs

---
+++ getAuthorDOI.py
@@ -0,0 +1,27 @@
+import imp
+import scopus
+from scopus import AuthorRetrieval
+from scopus import AbstractRetrieval
+import pandas as pd
+
+# put the ID of the author you want to get the DOIs
+author_id = 18439033600
+
+# pull the data
+au = AuthorRetrieval(author_id)
+
+# parse the data as DataFrame
+eids = pd.DataFrame(au.get_document_eids(refresh=False))
+
+# direct the stdout print to text file
+import sys
+sys.stdout = open('doi{}.txt'.format(author_id),'wt')
+
+# get the number of DOI pulled
+n=len(eids[0])
+
+for i in range(n):
+    # pull the details of the article, FULL means get all the fields.
+    ab = AbstractRetrieval(eids[0][i], view='FULL')
+    print(ab.doi, end=",")
+    print(ab.scopus_link)
