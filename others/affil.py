2d057b622f924d570b71bbb08d798dd1c13196ef
ntudpi <38546484+ntudpi@users.noreply.github.com>
2018-12-10 14:47:46 +0800

    Update affil.py

--- others/affil.py
+++ others/affil.py
@@ -1,18 +1,13 @@
 import imp
 from scopus import ScopusSearch
 import pandas as pd
-s = ScopusSearch('AF-ID(60005510) AND SUBJAREA(nurs)', refresh=False)
+s = ScopusSearch('AF-ID(60005510) AND SUBJAREA(ceng)', refresh=False)
 
 res = pd.DataFrame(s.results)
 
 import sys
-sys.stdout = open('NanyangNursing.txt','wt')
+sys.stdout = open('doiScopus.txt','wt')
 
-for c in res.columns:
-	print(c, end="	")
-
-print()
-for i in range(5):
-	for j in range(len(res.columns)):
-		print(res.iloc[i,j], end="	")
-	print()
+for i in range(len(res)):
+        print(res.iloc[i,1], end=",")
+        print(res.iloc[i,0])
